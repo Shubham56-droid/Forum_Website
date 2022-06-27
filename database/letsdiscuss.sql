3103f32bca618adf3f04bcd77fc531a06f7ec7d9
Shubham Bawankar <71594948+Shubham56-droid@users.noreply.github.com>
2022-06-27 23:26:19 +0530

    Add files via upload

--- database/letsdiscuss.sql
+++ database/letsdiscuss.sql
@@ -1,1 +1,181 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 27, 2022 at 07:54 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.0.19
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `letsdiscuss`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `category_id` int(8) NOT NULL,
+  `category_name` varchar(255) NOT NULL,
+  `category_description` text NOT NULL,
+  `category_time` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`category_id`, `category_name`, `category_description`, `category_time`) VALUES
+(1, 'python', 'Python is a general purpose, dynamic, high-level, and interpreted programming language. It supports Object Oriented programming approach to develop applications. It is simple and easy to learn and provides lots of high-level data structures.', '2022-06-20 10:35:04'),
+(2, 'javascript', 'JavaScript is interpreted object based scripting language which is used for programming web application.It is used along side of html and css.', '2022-06-20 10:41:48'),
+(3, 'java', 'The Java programming language was developed in the early 1990s by Sun Microsystem. Java is an object-oriented, simple, efficient, robust, and general-purpose programming language. It is primarily used for web-based enterprise applications.', '2022-06-20 12:02:59'),
+(4, 'django', 'Django is a high-level Python web framework that encourages rapid development and clean, pragmatic design. Built by experienced developers, it takes care of much of the hassle of web development, so you can focus on writing your app.', '2022-06-20 23:48:28'),
+(5, 'c++', 'C++ is a cross-platform language that can be used to create high-performance applications.C++ can be found in today\'s operating systems, Graphical User Interfaces, and embedded systems.C++ is an object-oriented programming language.', '2022-06-20 23:51:28'),
+(6, 'c', 'C is a general-purpose, high-level language that was originally developed by Dennis M. Ritchie to develop the UNIX operating system at Bell Labs. C was originally first implemented on the DEC PDP-11 computer in 1972.', '2022-06-20 23:56:19'),
+(7, 'php', 'PHP is a server scripting language, and a powerful tool for making dynamic and interactive Web pages.\r\nPHP is a widely-used, free, and efficient alternative to competitors such as Microsoft\'s ASP.', '2022-06-20 23:58:06');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `comment_id` int(10) NOT NULL,
+  `comment_content` text NOT NULL,
+  `thread_id` int(10) NOT NULL,
+  `comment_time` datetime NOT NULL DEFAULT current_timestamp(),
+  `comment_user_id` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+CREATE TABLE `contact` (
+  `sendername` varchar(20) NOT NULL,
+  `senderphone` varchar(15) NOT NULL,
+  `sendermessage` text NOT NULL,
+  `senderemail` varchar(30) NOT NULL,
+  `senderid` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `threads`
+--
+
+CREATE TABLE `threads` (
+  `thread_id` int(20) NOT NULL,
+  `thread_subject` varchar(300) NOT NULL,
+  `thread_description` text NOT NULL,
+  `thread_cat_id` int(20) NOT NULL,
+  `thread_user_id` int(20) NOT NULL,
+  `thread_time` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(8) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `useremail` varchar(30) NOT NULL,
+  `userpass` varchar(255) NOT NULL,
+  `userinstitude` text NOT NULL,
+  `jointime` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`category_id`);
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`comment_id`);
+
+--
+-- Indexes for table `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`senderid`);
+
+--
+-- Indexes for table `threads`
+--
+ALTER TABLE `threads`
+  ADD PRIMARY KEY (`thread_id`);
+ALTER TABLE `threads` ADD FULLTEXT KEY `thread_subject` (`thread_subject`,`thread_description`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `useremail` (`useremail`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `category_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `comment_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
+
+--
+-- AUTO_INCREMENT for table `contact`
+--
+ALTER TABLE `contact`
+  MODIFY `senderid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `threads`
+--
+ALTER TABLE `threads`
+  MODIFY `thread_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
